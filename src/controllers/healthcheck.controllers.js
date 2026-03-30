a4c822b11233abca9dc42d521c8a59557d3c4dbc
Krishnavamsi-codes <vamsi9398672280@gmail.com>
2026-03-30 23:32:23 +0530

    Added asynchandler to avoid manual try catch

--- src/controllers/healthcheck.controllers.js
+++ src/controllers/healthcheck.controllers.js
@@ -1,12 +1,14 @@
 import {ApiResponse} from "../utils/api-response.js"
+import { asynchandler } from "../utils/async-handler.js";
 
-const healthcheck=(req,res)=>
-{
-    try{
-        res.status(200).json(new ApiResponse(200,{message : "Server is running"}));
-    }catch(error){
-
-    };
-};
-
+//  const healthcheck=async (req,res,next)=>
+// {
+//     try{
+//         const user=await getUserFromDB()
+//         res.status(200).json(new ApiResponse(200,{message : "Server is running"}));
+//     }catch(error){
+//         next(error);
+//     };
+// }; this is manual try catch block instead of this we can do an asynceventhandler which takes the request handler as input and returns a funciton which resolves that event using the request handler and if any error comes it gets caught
+const healthcheck=asynchandler(async (req,res,next) =>{res.status(200).json(new ApiResponse(200,{message:"Server is running"}))})
 export {healthcheck};
