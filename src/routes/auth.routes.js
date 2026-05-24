f68df922914460f1384c142292c74d9ae4692e68
Krishnavamsi-codes <vamsi9398672280@gmail.com>
2026-05-24 13:20:20 +0530

    Added custom validations for Registering a user

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,8 @@
 import Router from "express"
 import {registeruser} from "../controllers/auth.controllers.js"
 const router=Router()
+import { validate } from "../middlewares/validator.middleware.js"
+import {userRegisterValidator} from "../validators/index.js"
 
-router.route("/register").post(registeruser)
+router.route("/register").post(userRegisterValidator(),validate,registeruser)
 export default router
---
+++ src/validators/index.js
@@ -0,0 +1,27 @@
+import {body} from "express-validator"
+
+const userRegisterValidator=()=>{
+    return[
+        body("email")
+        .trim()
+        .notEmpty()
+        .withMessage("e-mail Field is Empty.Please fill it")
+        .isEmail()
+        .withMessage("Not a valid e-mail @"),
+        body("username")
+        .trim()
+        .notEmpty()
+        .withMessage("Username Field is Empty.Please fill it")
+        .isLength({min:3})
+        .withMessage("Username must be atleast 3 characters long"),
+        body("password")
+        .trim()
+        .notEmpty()
+        .withMessage("Password Field is Empty.Please fill it"),
+        body("Fullname")
+        .optional().trim()
+
+
+    ]
+}
+export default {userRegisterValidator};
